abe2058ff5757f54aa382667fee6077595ca91a4
DanyMes <115036247+DanyMes@users.noreply.github.com>
2024-05-22 20:34:38 +0200

    Add files via upload

--- ProjektHotel.cpp
+++ ProjektHotel.cpp
@@ -5,15 +5,23 @@
 
 #include "ProjektHotel.h"
 #include "Unit2.h"
-#include<Registry.hpp>
+#include "XML.h"
+#include "JSON.h"
+#include "Database.h"
+#include "CustomFormat.h"
+#include "UnitTCPclient.h"
+#include "UnitUDPclient.h"
+#include "Zaposlenici.h"
+#include <Registry.hpp>
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 #pragma resource "*.dfm"
 TForm1 *Form1;
 //---------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------
 __fastcall TForm1::TForm1(TComponent* Owner)
 	: TForm(Owner)
-
 {
 	translation["LabelHotel"] =	{
 		{
@@ -23,8 +31,8 @@ __fastcall TForm1::TForm1(TComponent* Owner)
 	};
 	translation["LabelKorisnik"] =	{
 		{
-			{"EN", "User"},
-			{"HR", "Korisnik"}
+			{"EN", "Employee"},
+			{"HR", "Zaposlenik"}
 		}
 	};
 	translation["LabelLozinka"] =	{
@@ -54,32 +62,31 @@ __fastcall TForm1::TForm1(TComponent* Owner)
 		}
 	};
 
-	TRegistry *Registry = new TRegistry;
-	Registry->RootKey = HKEY_CURRENT_USER;
-	UnicodeString KLJUC = "Software\\appHotel";
-	// ako ključ postoji i ako ga se može otvorit...
-	if(Registry->OpenKey(KLJUC, false)){
-	Left = Registry->ReadInteger("X1");
-	Top = Registry->ReadInteger("Y1");
-	Width = Registry->ReadInteger("X2");
-	Height = Registry->ReadInteger("Y2");
+	translation["CheckBoxZapamti"] =	{
+		{
+			{"EN", "Remember me"},
+			{"HR", "Zapamti me"}
+		}
+	};
 
-	// Pročitaj font i veličinu fonta
-		UnicodeString FontName = Registry->ReadString("FontName");
-		int FontSize = Registry->ReadInteger("FontSize");
+	translation["Button2"] =	{
+		{
+			{"EN", "Font size"},
+			{"HR", "Veličina fonta"}
+		}
+	};
 
-		// Postavi font
-		Font->Name = FontName;
-		Font->Size = FontSize;
+	translation["ButtonSetColorClick"] =	{
+		{
+			{"EN", "Background color"},
+			{"HR", "Boja pozadine"}
+		}
+	};
 
-		// Pročitaj boju
-		TColor backgroundColor = (TColor)Registry->ReadInteger("BackgroundColor");
-		Color = backgroundColor;
-	Registry->CloseKey();
-}
-delete Registry;
 
-}
+};
+
+
 //---------------------------------------------------------------------------
 
 void translateForm(TForm* Form, String Language, const std::map<String, std::map<String, String>>& translation){
@@ -95,89 +102,213 @@ void translateForm(TForm* Form, String Language, const std::map<String, std::map
 //---------------------------------------------------------------------------
 void __fastcall TForm1::ComboBox1Change(TObject *Sender)
 {
-	translateForm(this, ComboBox1->Text, translation);
+// Dobivanje odabranog jezika iz ComboBoxa
+    String selectedLanguage = ComboBox1->Text;
+
+    // Ažuriranje prikaza forme
+    translateForm(this, selectedLanguage, translation);
+
+    // Spremanje odabranog jezika u INI datoteku
+    TIniFile* iniFile = new TIniFile("settings.ini");
+    iniFile->WriteString("Language", "SelectedLanguage", selectedLanguage);
+	delete iniFile;
 }
 //---------------------------------------------------------------------------
 
-
 void __fastcall TForm1::ButtonRegClick(TObject *Sender)
 {
+  // Postavljanje teksta dobrodošlice na Form2
+    Form2->LabelDobrodosli->Caption = "Dobrodošao, " + EditKorisnik->Text + "!";
 
-	Form2->LabelDobrodosli->Caption = "Dobrodošao, " + EditKorisnik->Text + "!";
+    // Provjera je li označen CheckBoxZapamti
+    if (CheckBoxZapamti->Checked)
+    {
+        // Spremanje unesenih podataka u Windows registar
+        TRegistry *Registry = new TRegistry;
+        Registry->RootKey = HKEY_CURRENT_USER;
+        UnicodeString KLJUC = "Software\\appHotel";
+        if (Registry->OpenKey(KLJUC, true))
+        {
+            Registry->WriteString("Korisnik", EditKorisnik->Text);
+            Registry->WriteString("Lozinka", EditLozinka->Text);
+            Registry->CloseKey(); // Zatvori ključ
+		}
+        delete Registry; // Oslobađanje resursa
+    }
+    else
+    {
+        // Ako CheckBoxZapamti nije označen, očisti polja za unos
+		EditKorisnik->Text = "";
+        EditLozinka->Text = "";
+    }
+
+	// Prikazivanje Form2
 	Form2->ShowModal();
 }
 //---------------------------------------------------------------------------
 
-void __fastcall TForm1::FormClose(TObject *Sender, TCloseAction &Action)
+
+//---------------------------------------------------------------------------
+void __fastcall TForm1::FormCreate(TObject *Sender)
 {
-	  TIniFile* ini = new TIniFile (GetCurrentDir() + "\\settings.ini");
-	ini->WriteInteger ("Main Window", "Left", Left);
-	ini->WriteInteger ("Main Window", "Top", Top);
-	ini->WriteInteger ("Main Window", "Width", Width);
-	ini->WriteInteger ("Main Window", "Height", Height);
+	 HINSTANCE ResourceDll;
+	 if((ResourceDll = LoadLibrary(L"mydll.dll"))==NULL) {
+		 ShowMessage("Cannot load mydll.dll!");
+		 return;
+	 }
+	 Image1->Picture->Bitmap->LoadFromResourceName((int)ResourceDll, "slika");
+	 FreeLibrary(ResourceDll);
 
-	// Spremanje postavki fonta
-	ini->WriteString("Font", "Name", currentFont->Name);
-	ini->WriteInteger("Font", "Size", currentFont->Size);
 
-	// Spremanje postavki boje pozadine
-	ini->WriteInteger("Background", "Color", backgroundColor);
+	 // Čitanje postavki iz INI datoteke za prozor
+	TIniFile* ini = new TIniFile(GetCurrentDir() + "\\settings.ini");
+	Left = ini->ReadInteger("Main Window", "Left", 0);
+	Top = ini->ReadInteger("Main Window", "Top", 0);
+	Width = ini->ReadInteger("Main Window", "Width", 700);
+	Height = ini->ReadInteger("Main Window", "Height", 500);
 
-	delete ini;
+	// Čitanje postavki boje pozadine iz INI datoteke
+	TColor backgroundColor = (TColor)ini->ReadInteger("Appearance", "BackgroundColor", clGreen);
 
+	// Čitanje odabranog jezika iz INI datoteke
+	String selectedLanguage = ini->ReadString("Language", "SelectedLanguage", "");
+
+	delete ini; // Oslobađanje resursa
+
+	// Postavljanje boje pozadine forme na temelju pročitane vrijednosti iz INI datoteke
+	Color = backgroundColor;
+
+	// Postavljanje odabranog jezika u ComboBox
+	ComboBox1->ItemIndex = ComboBox1->Items->IndexOf(selectedLanguage);
+
+	// Ako je odabrani jezik pronađen, ažuriramo prikaz forme
+	if (selectedLanguage != "")
+		translateForm(this, selectedLanguage, translation);
+
+	// Čitanje unesenih podataka iz Windows registra
 	TRegistry *Registry = new TRegistry;
 	Registry->RootKey = HKEY_CURRENT_USER;
 	UnicodeString KLJUC = "Software\\appHotel";
-	if(Registry->OpenKey(KLJUC, true)){ // izradi ključ (ukoliko ne postoji)
-	Registry->WriteInteger("X1", Left);
-	Registry->WriteInteger("Y1", Top);
-	Registry->WriteInteger("X2", Width);
-	Registry->WriteInteger("Y2", Height);
+	if (Registry->OpenKey(KLJUC, false)) // Otvori ključ samo za čitanje
+	{
+		EditKorisnik->Text = Registry->ReadString("Korisnik");
+		EditLozinka->Text = Registry->ReadString("Lozinka");
+		Registry->CloseKey(); // Zatvori ključ
+	}
+	delete Registry; // Oslobađanje resursa
 
-	// Spremi postavke fonta
-		Registry->WriteString("FontName", Font->Name);
-		Registry->WriteInteger("FontSize", Font->Size);
 
-		// Spremi postavke boje
-		Registry->WriteInteger("BackgroundColor", Color);
-	Registry->CloseKey(); // zatvori ključ
+    // Čitanje veličine fonta iz INI datoteke
+    TIniFile* fontIni = new TIniFile(GetCurrentDir() + "\\settings.ini");
+    int fontSize = fontIni->ReadInteger("Font", "FontSize", Font->Size);
+    delete fontIni;
+
+    // Ažuriranje veličine fonta na cijeloj formi
+    Font->Size = fontSize;
+
+    // Odmah ažuriraj veličinu fonta na FormZaposlenici
+    if (FormZaposlenici) {
+        FormZaposlenici->RefreshSettings();
+	}
 }
-delete Registry;
-}
-//---------------------------------------------------------------------------
-
-void __fastcall TForm1::FormCreate(TObject *Sender)
+ //------------------------------------------------------------------
+void __fastcall TForm1::FormClose(TObject *Sender, TCloseAction &Action)
 {
-	  TIniFile* ini = new TIniFile (GetCurrentDir() + "\\settings.ini");
-	Left = ini->ReadInteger ("Main Window", "Left", 0);
-	Top = ini->ReadInteger ("Main Window", "Top", 0);
-	Width = ini->ReadInteger ("Main Window", "Width", 700);
-	Height = ini->ReadInteger ("Main Window", "Height", 500);
+	// Čuvanje postavki prozora u INI datoteku
+	TIniFile* ini = new TIniFile(GetCurrentDir() + "\\settings.ini");
+	ini->WriteInteger("Main Window", "Left", Left);
+	ini->WriteInteger("Main Window", "Top", Top);
+	ini->WriteInteger("Main Window", "Width", Width);
+	ini->WriteInteger("Main Window", "Height", Height);
 
-	// Èitanje postavki fonta
-	currentFont = new TFont();
-	currentFont->Name = ini->ReadString("Font", "Name", "Helvetica");
-	currentFont->Size = ini->ReadInteger("Font", "Size", 8);
+	// Čuvanje postavki boje pozadine u INI datoteku
+	ini->WriteInteger("Appearance", "BackgroundColor", Color);
 
-	delete ini;
+	// Čuvanje postavki fonta u INI datoteku
+	ini->WriteInteger("Font", "FontSize", Font->Size);
+	ini->WriteInteger("Font", "FontColor", Font->Color);
 
-	// Postavljanje fonta i boje pozadine
-	Font->Name = "Arial";
-	Color = clTeal;
+	// Čuvanje odabranog jezika u INI datoteku
+	String selectedLanguage = ComboBox1->Text;
+	ini->WriteString("Language", "SelectedLanguage", selectedLanguage);
+
+	delete ini; // Oslobađanje resursa
+
+    // Čuvanje unesenih podataka u Windows registar
+    TRegistry *Registry = new TRegistry;
+    Registry->RootKey = HKEY_CURRENT_USER;
+    UnicodeString KLJUC = "Software\\appHotel";
+    if (Registry->OpenKey(KLJUC, true)) // Otvori ključ za pisanje
+    {
+        Registry->WriteString("Korisnik", EditKorisnik->Text);
+        Registry->WriteString("Lozinka", EditLozinka->Text);
+        Registry->CloseKey(); // Zatvori ključ
+    }
+    delete Registry; // Oslobađanje resursa
 }
-
 //---------------------------------------------------------------------------
-
 
 void __fastcall TForm1::CheckBoxPasswordClick(TObject *Sender)
 {
-	EditLozinka->PasswordChar = CheckBoxPassword->Checked ? '\0' : '*';
+		EditLozinka->PasswordChar = CheckBoxPassword->Checked ? '\0' : '*';
 }
 //---------------------------------------------------------------------------
 
 void __fastcall TForm1::FormShow(TObject *Sender)
 {
-	  EditLozinka->PasswordChar = CheckBoxPassword->Checked ? '\0' : '*';
+		  EditLozinka->PasswordChar = CheckBoxPassword->Checked ? '\0' : '*';
+
 }
 //---------------------------------------------------------------------------
+
+void __fastcall TForm1::ButtonSetColorClickClick(TObject *Sender)
+{
+// Prikaži dijalog za odabir boje
+	if (ColorDialog1->Execute()) {
+		// Prikaži odabranu boju u poruci
+		ShowMessage("Boja je uspješno odabrana !");
+
+		// Postavi odabranu boju kao boju pozadine forme
+		Color = ColorDialog1->Color;
+
+		// Spremi odabranu boju u INI datoteku
+		TIniFile* ini = new TIniFile(GetCurrentDir() + "\\settings.ini");
+		ini->WriteInteger("Appearance", "BackgroundColor", ColorDialog1->Color);
+		delete ini;
+
+         // Odmah ažuriraj boju pozadine na FormZaposlenici
+        if (FormZaposlenici) {
+            FormZaposlenici->RefreshSettings();
+		}
+	}
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button1Click(TObject *Sender)
+{
+	 FormZaposlenici->ShowModal();
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button2Click(TObject *Sender)
+{
+	 // Čitanje unosa korisnika iz Edit polja
+    int fontSize = StrToIntDef(EditFontSize->Text, 0);
+
+    // Spremanje unosa u INI datoteku
+    TIniFile* iniFile = new TIniFile("settings.ini");
+    iniFile->WriteInteger("Font", "FontSize", fontSize);
+    delete iniFile;
+
+    // Ažuriranje veličine fonta na cijeloj formi
+    Font->Size = fontSize;
+
+    // Odmah ažuriraj veličinu fonta na FormZaposlenici
+    if (FormZaposlenici) {
+        FormZaposlenici->Font->Size = fontSize; // Ovdje postavljamo veličinu fonta na FormZaposlenici
+	}
+}
+//---------------------------------------------------------------------------
+
+
 
