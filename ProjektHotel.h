0d690a223c245c20f7411c9d7060199f974c298b
dankom <115036247+dankom17@users.noreply.github.com>
2024-06-01 16:15:37 +0200

    Add files via upload

--- ProjektHotel.h
+++ ProjektHotel.h
@@ -7,6 +7,11 @@
 #include <Vcl.Controls.hpp>
 #include <Vcl.StdCtrls.hpp>
 #include <Vcl.Forms.hpp>
+#include <Vcl.Dialogs.hpp>
+#include <Vcl.ExtCtrls.hpp>
+#include "uTPLb_BaseNonVisualComponent.hpp"
+#include "uTPLb_Codec.hpp"
+#include <System.SysUtils.hpp>
 #include <map>
 //---------------------------------------------------------------------------
 class TForm1 : public TForm
@@ -20,15 +25,26 @@ __published:	// IDE-managed Components
 	TComboBox *ComboBox1;
 	TButton *ButtonReg;
 	TCheckBox *CheckBoxPassword;
+	TButton *Button1;
+	TColorDialog *ColorDialog1;
+	TButton *ButtonSetColorClick;
+	TEdit *EditFontSize;
+	TButton *Button2;
+	TCheckBox *CheckBoxZapamti;
+	TImage *Image1;
+	TCodec *Codec1;
 	void __fastcall ComboBox1Change(TObject *Sender);
-	void __fastcall ButtonRegClick(TObject *Sender);
 	void __fastcall FormClose(TObject *Sender, TCloseAction &Action);
 	void __fastcall FormCreate(TObject *Sender);
 	void __fastcall CheckBoxPasswordClick(TObject *Sender);
 	void __fastcall FormShow(TObject *Sender);
+	void __fastcall ButtonSetColorClickClick(TObject *Sender);
+	void __fastcall Button1Click(TObject *Sender);
+	void __fastcall Button2Click(TObject *Sender);
+	void __fastcall ButtonRegClick(TObject *Sender);
 private:
-	  TColor backgroundColor;
-      TFont *currentFont;
+  TFont  *currentFont;
+  TColor backgroundColor;
 	// User declarations
 public:		// User declarations
 	std::map<String, std::map<String, String>> translation;
